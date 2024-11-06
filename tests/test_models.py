195768b536348567eed5f6b52997f9a684ed60fd
jxsaw <jxsaw@hotmail.com>
2024-11-07 00:53:21 +0800

    Refactor code for linting compliance

--- tests/test_models.py
+++ tests/test_models.py
@@ -112,7 +112,7 @@ class TestProductModel(unittest.TestCase):
         self.assertIsNotNone(product.id)    # validate that create method should have saved the product and generated an id
         # fetch it back
         found_product = Product.find(product.id)
-        self.assertEqual(found_product.id, product.id)                      # These assertions confirm that the retrieved 
+        self.assertEqual(found_product.id, product.id)                      # These assertions confirm that the retrieved
         self.assertEqual(found_product.name, product.name)                  # products (found_product) matches the original
         self.assertEqual(found_product.description, product.description)    # ones (product). Each attribute is checked to
         self.assertEqual(found_product.price, product.price)                # ensure data integrity.
@@ -120,13 +120,13 @@ class TestProductModel(unittest.TestCase):
     def test_update_a_product(self):
         """It should Update a Product"""
         product = ProductFactory()
-        product.id = None                   
+        product.id = None
         product.create()
         self.assertIsNotNone(product.id)
         # Change it an save it
-        product.description = "testing" # change product description
-        original_id = product.id        # original_id is saved to ensure id remains consistent after updating
-        product.update()                # save
+        product.description = "testing"  # change product description
+        original_id = product.id         # original_id is saved to ensure id remains consistent after updating
+        product.update()                 # save
         self.assertEqual(product.id, original_id)
         self.assertEqual(product.description, "testing")
         # Fetch it back and make sure the id hasn't changed
