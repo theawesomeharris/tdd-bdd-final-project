64251e87055cc40cdb854f8d278f97b77da51245
jxsaw <jxsaw@hotmail.com>
2024-11-06 06:23:26 -0500

    added test cases for functions in model.py

--- tests/test_models.py
+++ tests/test_models.py
@@ -104,3 +104,82 @@ class TestProductModel(unittest.TestCase):
     #
     # ADD YOUR TEST CASES HERE
     #
+    def test_read_a_product(self):
+        """It should read a product"""
+        product = ProductFactory()            # create a product instance
+        product.id = None                   # set the product id to None to simulate a product that hasn't been assigned an id
+        product.create()                    # save the product to the database and assigns it a unique id
+        self.assertIsNotNone(product.id)    # validate that create method should have saved the product and generated an id
+        # fetch it back
+        found_product = Product.find(product.id)
+        self.assertEqual(found_product.id, product.id)                      # These assertions confirm that the retrieved 
+        self.assertEqual(found_product.name, product.name)                  # products (found_product) matches the original
+        self.assertEqual(found_product.description, product.description)    # ones (product). Each attribute is checked to
+        self.assertEqual(found_product.price, product.price)                # ensure data integrity.
+
+    def test_update_a_product(self):
+        """It should Update a Product"""
+        product = ProductFactory()
+        product.id = None                   
+        product.create()
+        self.assertIsNotNone(product.id)
+        # Change it an save it
+        product.description = "testing" # change product description
+        original_id = product.id        # original_id is saved to ensure id remains consistent after updating
+        product.update()                # save
+        self.assertEqual(product.id, original_id)
+        self.assertEqual(product.description, "testing")
+        # Fetch it back and make sure the id hasn't changed
+        # but the data did change
+        products = Product.all()                                # retrieves all products using .all()
+        self.assertEqual(len(products), 1)                      # assert there is only one product in storage
+        self.assertEqual(products[0].id, original_id)           # the id matches original_id
+        self.assertEqual(products[0].description, "testing")    # the description is updated to "testing"
+
+    def test_list_all_products(self):
+        """It should List all Products in the database"""
+        products = Product.all()
+        self.assertEqual(products, [])
+        # Create 5 Products
+        for _ in range(5):
+            product = ProductFactory()
+            product.create()
+        # See if we get back 5 products
+        products = Product.all()
+        self.assertEqual(len(products), 5)
+
+    def test_find_by_name(self):
+        """It should Find a Product by Name"""
+        products = ProductFactory.create_batch(5)
+        for product in products:
+            product.create()
+        name = products[0].name
+        count = len([product for product in products if product.name == name])
+        found = Product.find_by_name(name)
+        self.assertEqual(found.count(), count)
+        for product in found:
+            self.assertEqual(product.name, name)
+
+    def test_find_by_availability(self):
+        """It should Find Products by Availability"""
+        products = ProductFactory.create_batch(10)
+        for product in products:
+            product.create()
+        available = products[0].available
+        count = len([product for product in products if product.available == available])
+        found = Product.find_by_availability(available)
+        self.assertEqual(found.count(), count)
+        for product in found:
+            self.assertEqual(product.available, available)
+
+    def test_find_by_category(self):
+        """It should Find Products by Category"""
+        products = ProductFactory.create_batch(10)
+        for product in products:
+            product.create()
+        category = products[0].category
+        count = len([product for product in products if product.category == category])
+        found = Product.find_by_category(category)
+        self.assertEqual(found.count(), count)
+        for product in found:
+            self.assertEqual(product.category, category)
